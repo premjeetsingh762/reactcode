af567721c127c313cb18ba45858cd7de42e7cc92
premjeetsingh762 <49304470+premjeetsingh762@users.noreply.github.com>
2020-08-13 20:37:55 +0530

    Add files via upload

---
+++ List.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+function List(){
+     return(
+         <ol>
+             <li>Vinod</li>
+             <li>Bahadur</li>
+             <li>thapa</li>
+             <li>thapa Technical</li>
+             <li>Plz Subscribe</li>
+         </ol>
+     );
+
+     )
+
+export default List;
